e75642a52a19692d41fe433cc747e931893c0500
SakquerSchool1409 <s-nikonov@school1409.ru>
2025-05-22 21:05:07 +0300

    Add files via upload

---
+++ mapFigma/flour4/script.js
@@ -0,0 +1,34 @@
+const buttonFlour1 = document.querySelector('.text-wrapper-41')
+const buttonFlour2 = document.querySelector('.text-wrapper-42')
+const buttonFlour3 = document.querySelector('.text-wrapper-43')
+const swiftFlour = document.querySelector('.overlap-15')
+
+const bod = document.querySelector('.overlap')
+
+// animation
+document.addEventListener('DOMContentLoaded', () => {
+    bod.style.transform = 'translateY(1920px)'
+})
+
+//navigation
+const swiftMove = (y, flour) => {
+    bod.style.transform = 'translateY(-1920px)'
+    swiftFlour.style.transform = `translateY(${y}px)`
+    setTimeout(() => {location.href = `../flour${flour}/index.html`}, 500)
+}
+
+buttonFlour1.addEventListener('click', () => {
+    swiftMove(-235, 1)
+})
+
+buttonFlour2.addEventListener('click', () => {
+    swiftMove(-155, 2)
+
+})
+
+buttonFlour3.addEventListener('click', () => {
+    swiftMove(-75, 3)
+})
+
+
+
---
+++ mapFigma/main.py
@@ -0,0 +1,10 @@
+import webview
+
+
+
+app = webview.create_window(
+    title="map 1409",
+    html="file:///C:/Users/sakquer/Desktop/mapFigma/flour1/index.html"
+)
+
+webview.start()
